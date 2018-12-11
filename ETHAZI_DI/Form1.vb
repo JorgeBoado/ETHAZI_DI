0251cfc1fda2779022bb2cc79299368174e3c996
JorgeBoado <jorge.boadogo@elorrieta-errekamari.com>
2018-12-11 08:59:58 +0100

    Segundo commit

--- ETHAZI_DI/Form1.vb
+++ ETHAZI_DI/Form1.vb
@@ -1,5 +1,6 @@
 ﻿Public Class Form1
-	Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-		MsgBox("Hellow")
+
+	Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+		'er
 	End Sub
 End Class
