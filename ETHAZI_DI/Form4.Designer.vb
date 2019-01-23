18bec4f550fd01ca1b1fa0a0ac83060cf6dc9942
AritzCelaya <aritz.celayaga@elorrieta-errekamari.com>
2019-01-23 14:23:25 +0100

    23/01/2019 final

--- ETHAZI_DI/Form4.Designer.vb
+++ ETHAZI_DI/Form4.Designer.vb
@@ -23,54 +23,39 @@ Partial Class Form4
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.CrystalReportViewer1 = New CrystalDecisions.Windows.Forms.CrystalReportViewer()
-        Me.CrystalReportViewer2 = New CrystalDecisions.Windows.Forms.CrystalReportViewer()
         Me.ComboBox1 = New System.Windows.Forms.ComboBox()
-        Me.SuspendLayout()
+        Me.SuspendLayout
         '
         'CrystalReportViewer1
         '
         Me.CrystalReportViewer1.ActiveViewIndex = -1
         Me.CrystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.CrystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default
-        Me.CrystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.CrystalReportViewer1.Location = New System.Drawing.Point(0, 0)
+        Me.CrystalReportViewer1.Location = New System.Drawing.Point(0, 39)
         Me.CrystalReportViewer1.Name = "CrystalReportViewer1"
-        Me.CrystalReportViewer1.Size = New System.Drawing.Size(905, 603)
+        Me.CrystalReportViewer1.Size = New System.Drawing.Size(905, 568)
         Me.CrystalReportViewer1.TabIndex = 0
-        '
-        'CrystalReportViewer2
-        '
-        Me.CrystalReportViewer2.ActiveViewIndex = -1
-        Me.CrystalReportViewer2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.CrystalReportViewer2.Cursor = System.Windows.Forms.Cursors.Default
-        Me.CrystalReportViewer2.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.CrystalReportViewer2.Location = New System.Drawing.Point(0, 0)
-        Me.CrystalReportViewer2.Name = "CrystalReportViewer2"
-        Me.CrystalReportViewer2.Size = New System.Drawing.Size(905, 603)
-        Me.CrystalReportViewer2.TabIndex = 1
         '
         'ComboBox1
         '
         Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(227, 48)
+        Me.ComboBox1.Location = New System.Drawing.Point(235, 12)
         Me.ComboBox1.Name = "ComboBox1"
         Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
         Me.ComboBox1.TabIndex = 2
         '
         'Form4
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(905, 603)
         Me.Controls.Add(Me.ComboBox1)
-        Me.Controls.Add(Me.CrystalReportViewer2)
         Me.Controls.Add(Me.CrystalReportViewer1)
         Me.Name = "Form4"
         Me.Text = "Form4"
-        Me.ResumeLayout(False)
+        Me.ResumeLayout(false)
 
-    End Sub
+End Sub
     Friend WithEvents CrystalReportViewer1 As CrystalDecisions.Windows.Forms.CrystalReportViewer
-    Friend WithEvents CrystalReportViewer2 As CrystalDecisions.Windows.Forms.CrystalReportViewer
     Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
 End Class
--- ETHAZI_DI/Form4.vb
+++ ETHAZI_DI/Form4.vb
@@ -15,10 +15,10 @@ Public Class Form4
     End Sub
 
     Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
-        Dim Informe As New rptPrueba()
+        Dim oInforme As New CrystalReport2
         ' establecer la fórmula de selección de registros
-        Informe.RecordSelectionFormula = "{Suppliers.Country} = '" & Me.ComboBox1.Text & "'"
-        Me.CrystalReportViewer1.ReportSource = Informe
+        oInforme.RecordSelectionFormula = "{lodging1.name} = '" & Me.ComboBox1.Text & "'"
+        Me.CrystalReportViewer1.ReportSource = oInforme
 
     End Sub
 End Class
